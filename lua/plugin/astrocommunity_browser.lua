004402384938ee3b07a173ae24f2cfd110533a34
Jon Kinney <jonkinney@gmail.com>
2024-08-27 00:44:20 -0500

    Remove superfluous code

--- lua/plugin/astrocommunity_browser.lua
+++ lua/plugin/astrocommunity_browser.lua
@@ -3,12 +3,4 @@ if vim.g.loaded_astrocommunity_browser then
 	return
 end
 
--- Load and setup the plugin
-require("astrocommunity_browser").setup()
-
--- Create the command
-vim.api.nvim_create_user_command("OpenAstroCommunityPluginURL", function()
-	require("astrocommunity_browser.commands").open_url()
-end, {})
-
 vim.g.loaded_astrocommunity_browser = true
