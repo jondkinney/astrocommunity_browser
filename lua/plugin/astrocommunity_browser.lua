2dd36dd003f699b2d7a88cf0b4751d8c872b62d8
Jon Kinney <jonkinney@gmail.com>
2024-08-27 00:42:46 -0500

    fix: auto load fix

--- lua/plugin/astrocommunity_browser.lua
+++ lua/plugin/astrocommunity_browser.lua
@@ -1,21 +1,14 @@
--- plugin/astrocommunity_browser.lua
-
-if vim.fn.has("nvim-0.7.0") == 0 then
-	vim.api.nvim_err_writeln("astrocommunity_browser requires at least nvim-0.7.0")
-	return
-end
-
 -- prevent loading file twice
-if vim.g.loaded_astrocommunity_browser == 1 then
+if vim.g.loaded_astrocommunity_browser then
 	return
 end
-vim.g.loaded_astrocommunity_browser = 1
 
 -- Load and setup the plugin
-local astrocommunity_browser = require("astrocommunity_browser")
-astrocommunity_browser.setup()
+require("astrocommunity_browser").setup()
 
 -- Create the command
 vim.api.nvim_create_user_command("OpenAstroCommunityPluginURL", function()
 	require("astrocommunity_browser.commands").open_url()
 end, {})
+
+vim.g.loaded_astrocommunity_browser = true
